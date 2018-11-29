295dad3310243c2bca46afdde2303afe111685d3
姚康明 <154518484@qq.com>
2018-11-29 16:15:44 +0800

    optimise.optimise.

--- src/main/java/com/github/util/ThreadPoolConfig.java
+++ src/main/java/com/github/util/ThreadPoolConfig.java
@@ -32,7 +32,7 @@ public class ThreadPoolConfig extends AsyncConfigurerSupport implements Scheduli
 	@Override
 	public void afterPropertiesSet() {
 		taskScheduler = new ThreadPoolTaskScheduler();
-		taskScheduler.setPoolSize(Runtime.getRuntime().availableProcessors() * 2);
+		taskScheduler.setPoolSize(Runtime.getRuntime().availableProcessors() * 32);
 		taskScheduler.setAwaitTerminationSeconds(60 * 60);
 		taskScheduler.setThreadNamePrefix("executor-");
 		taskScheduler.setWaitForTasksToCompleteOnShutdown(true);
